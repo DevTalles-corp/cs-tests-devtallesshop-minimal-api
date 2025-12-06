235eb12220c14e85c52b4b380100bab59cecaf11
Teddy Paz <pazteddy@gmail.com>
2025-12-06 11:47:12 -0400

    Add Customer and CustomerResponse models for customer management

---
+++ DevTallesShop.Api/Features/Customers/Models/Customer.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace DevTallesShop.Api.Features.Customers.Models;
+
+public class Customer
+{
+  public int Id { get; set; }
+  public string? Name { get; set; }
+  public string? Email { get; set; }
+
+  public Customer(int id, string name, string email)
+  {
+    Id = id;
+    Name = name;
+    Email = email;
+  }
+}
