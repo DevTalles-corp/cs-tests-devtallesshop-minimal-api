ef4328766465f90ad9f12c785246b45e08a4de63
Teddy Paz <pazteddy@gmail.com>
2025-12-08 12:06:49 -0400

    Add GetOrders method to InMemoryDataSeeder for order data seeding

--- DevTallesShop.Api/Data/InMemoryDataSeeder.cs
+++ DevTallesShop.Api/Data/InMemoryDataSeeder.cs
@@ -1,5 +1,6 @@
 using System;
 using DevTallesShop.Api.Features.Customers.Models;
+using DevTallesShop.Api.Features.Orders.Models;
 using DevTallesShop.Api.Features.Products.Models;
 
 namespace DevTallesShop.Api.Data;
@@ -17,5 +18,25 @@ public static class InMemoryDataSeeder
   {
     new Customer(1,"Devi DevTalles","devi@example.com"),
     new Customer(2,"Teddy Paz","teddy@example.com")
+  };
+
+  public static List<Order> GetOrders() => new()
+  {
+    new Order(
+      id: 1,
+      orderDate: DateTime.UtcNow.AddHours(-2),
+      total:30m,
+      customerId:1,
+      productId:1,
+      quantity:2
+    ),
+    new Order(
+      id: 2,
+      orderDate: DateTime.UtcNow.AddHours(-1),
+      total:120m,
+      customerId:2,
+      productId:3,
+      quantity:1
+    )
   };
 }
