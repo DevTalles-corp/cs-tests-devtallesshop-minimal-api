74c6f713f14c0a578ac55f6319a5b9be45d282fc
Teddy Paz <pazteddy@gmail.com>
2025-12-11 12:59:34 -0400

    Add ProductsEndpointsTests with GetAllProducts test and initial structure for product endpoint tests

---
+++ DevTallesShop.Api.Tests/Endpoints/ProductsEndpointsTests.cs
@@ -0,0 +1,56 @@
+using System;
+
+namespace DevTallesShop.Api.Tests.Endpoints;
+
+public class ProductsEndpointsTests
+{
+  [Fact]
+  public void GetAllProducts_ReturnsOkWithProducts()
+  {
+    var products = new[]
+    {
+      new Product(1,"Mouse",15m,true),
+      new Product(2,"Teclado",30m,true),
+      new Product(3,"Monitor",120m,false)
+    };
+    var serviceMock = new Mock<IProductService>();
+    serviceMock.Setup(s => s.GetAllProducts()).Returns(products);
+
+    var result = ProductsEndpoints.GetAll(serviceMock.Object);
+    var okResult = result.Should().BeOfType<Ok<IEnumerable<ProductResponse>>>().Subject;
+    // Console.WriteLine("=======");
+    // Console.WriteLine(okResult);
+    okResult.StatusCode.Should().Be(StatusCodes.Status200OK);
+    okResult.Value.Should().HaveCount(3);
+    okResult.Value.Should().AllSatisfy(p =>
+    {
+      p.Id.Should().BeGreaterThan(0);
+      p.Name.Should().NotBeNullOrEmpty();
+      p.Price.Should().BeGreaterThan(0);
+    });
+  }
+  [Fact(Skip = "Pendiente de implementación")]
+  public void GetProductById_ReturnsOKWhithProduct()
+  { }
+  [Fact(Skip = "Pendiente de implementación")]
+  public void GetProductById_ReturnsNotFound_WhenProductDoesNotExist()
+  { }
+  [Fact(Skip = "Pendiente de implementación")]
+  public void CreateProduct_ReturnsCreatedWithPayload()
+  { }
+  [Fact(Skip = "Pendiente de implementación")]
+  public void CreateProduct_ReturnsBadRequest_WhenPriceIsNegative()
+  { }
+  [Fact(Skip = "Pendiente de implementación")]
+  public void UpdateProduct_ReturnsOk_WhenUpdateSucceeds()
+  { }
+  [Fact(Skip = "Pendiente de implementación")]
+  public void UpdateProduct_ReturnsNotFound_WhenServiceReturnsFalse()
+  { }
+  [Fact(Skip = "Pendiente de implementación")]
+  public void DeleteProduct_ReturnsNoContent_WhenDeletionSucceeds()
+  { }
+  [Fact(Skip = "Pendiente de implementación")]
+  public void DeleteProduct_ReturnsNotFound_WhenDeletionFails()
+  { }
+}
